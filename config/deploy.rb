ab0ce548afb4d8aa205b2daf70c3f1b6ba727027
puu <secret>
2016-04-30 00:49:29 +0900

    added some changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,8 @@ set :deploy_to, '/var/www/deploy_test'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'run/sockets', 'public/system') # add
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -48,6 +49,7 @@ namespace :deploy do
       # end
 
       # add # gem install unicorn rack
+      # add # chown -R vagrant:vagrant www
     end
   end
 
