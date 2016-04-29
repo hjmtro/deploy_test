b63f712428c2a9b0290b456553d257accc183cae
puu <secret>
2016-04-29 18:27:22 +0900

    add git repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.5.0'
 
 set :application, 'deploy_test'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:hjmtro/deploy_test.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
